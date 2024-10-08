edf131a8e77d6733adec720606babfdd69084672
quratulain08 <141954239+quratulain08@users.noreply.github.com>
2024-10-08 21:55:41 +0500

    Published

--- src/models/RecommendationLetter.ts
+++ src/models/RecommendationLetter.ts
@@ -2,15 +2,45 @@ import mongoose, { Schema, model, models, Document } from 'mongoose';
 
 // Define the RecommendationLetter interface
 interface IRecommendationLetter extends Document {
-  studentId: mongoose.Types.ObjectId; // Use ObjectId for student reference
-  letter: string;
+  studentName: string;
+  fatherName: string;
+  cnic: string;
+  registrationNo: string;
+  cgpa: string;
+  degree: string;
+  semester: string;
+  cellNo: string;
+  whatsAppNo: string;
+  email: string;
+  hrManagerName: string;
+  hrPhoneNo: string;
+  hrEmail: string;
+  postalAddress: string;
+  studentIdFile: any;
+  transcriptFile: any;
+  agreementAccepted: boolean;
   date: Date;
 }
 
 // Define the RecommendationLetter schema
 const RecommendationLetterSchema = new Schema<IRecommendationLetter>({
-  studentId: { type: Schema.Types.ObjectId, required: true, ref: 'User' }, // Use Schema.Types.ObjectId
-  letter: { type: String, required: true },
+  studentName: { type: String, required: true },
+  fatherName: { type: String, required: true },
+  cnic: { type: String, required: true },
+  registrationNo: { type: String, required: true },
+  cgpa: { type: String, required: true },
+  degree: { type: String, required: true },
+  semester: { type: String, required: true },
+  cellNo: { type: String, required: true },
+  whatsAppNo: { type: String, required: true },
+  email: { type: String, required: true },
+  hrManagerName: { type: String, required: true },
+  hrPhoneNo: { type: String, required: true },
+  hrEmail: { type: String, required: true },
+  postalAddress: { type: String, required: true },
+  studentIdFile: { type: Buffer, required: true },
+  transcriptFile: { type: Buffer, required: true },
+  agreementAccepted: { type: Boolean, required: true },
   date: { type: Date, default: Date.now },
 });
 
